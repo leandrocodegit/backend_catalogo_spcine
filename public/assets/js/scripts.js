b94c26fd97608cea3f6dd7f0d2a7809b74118ef0
leandrocodegit <lpoliveira.ti@gmail.com>
2023-09-02 21:48:56 -0300

    Adicionado endpoit regiao catalogo

--- public/assets/js/scripts.js
+++ public/assets/js/scripts.js
@@ -1,21 +1,5 @@
-// Sidebar mobile open
+L.Map.mergeOptions({twoFingerZoom:L.Browser.touch&&!L.Browser.android23});L.Map.TwoFingerZoom=L.Handler.extend({statics:{ZOOM_OUT_THRESHOLD:100},_touchStartTime:[],addHooks:function(){L.DomEvent.on(this._map._container,"touchstart",this._onTouchStart,this);L.DomEvent.on(this._map._container,"touchend",this._onTouchEnd,this)},removeHooks:function(){L.DomEvent.off(this._map._container,"touchstart",this._onTouchStart,this);L.DomEvent.off(this._map._container,"touchend",this._onTouchEnd,this)},_onTouchStart:function(e){var touches=e.touches;if(touches.length!==2){return}this._touchStartTime=new Date},_onTouchEnd:function(e){var map=this._map,touches=e.changedTouches;if(!this._touchStartTime){return}L.DomEvent.preventDefault(e);if(new Date-this._touchStartTime<=L.Map.TwoFingerZoom.ZOOM_OUT_THRESHOLD){this._touchStartTime=null;map.zoomOut()}}});L.Map.addInitHook("addHandler","twoFingerZoom",L.Map.TwoFingerZoom);
 
-function openLeftMenu() {
-    document.getElementById("leftMenu").style.display = "block";
-  }
-  
-  function closeLeftMenu() {
-    document.getElementById("leftMenu").style.display = "none";
-  }
-  
-  function openRightMenu() {
-    document.getElementById("rightMenu").style.display = "block";
-  }
-  
-  function closeRightMenu() {
-    document.getElementById("rightMenu").style.display = "none";
-  }
-  
   // Tooltip css
   
   var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
