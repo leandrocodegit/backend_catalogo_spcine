77ac3750677d6f7e9ea3abca63b75c28349956f2
leandrocodegit <lpoliveira.ti@gmail.com>
2023-09-29 15:16:52 -0300

    Criado convert imagem

--- app/Models/util/ImagemUtil.php
+++ app/Models/util/ImagemUtil.php
@@ -7,17 +7,11 @@ use Illuminate\Support\Facades\Storage;
 
 class ImagemUtil
 {
-
     public function __construct()
     {
     }
 
-    public static function convert($imagem)
-
-    {
-
-        $output = new \Symfony\Component\Console\Output\ConsoleOutput();
-
+    public static function convert($imagem){
 
         $imagemDB = Imagem::firstWhere('id', $imagem->id);
 
@@ -25,21 +19,15 @@ class ImagemUtil
         $originalImagePath = $imagemDB->catalogo_id . '/' . uniqid() . '.webp';
         $outputImagePath = 'imagens/' . $originalImagePath;
 
-        $output->write('$outputImagePath = ' . 'imagens/' . $originalImagePath);
-        $output->write('              ');
-
-        $output->write('$imagemDB->url = ' . 'imagens/' . $imagemDB->url);
-
-       // sleep(2);
-
-        if (Storage::disk('public')->exists('imagens/' . $imagem->url)) {
-            exec("cwebp $inputImagePath -o $outputImagePath");
-            Storage::disk('public')->delete($inputImagePath);
-            $imagemDB->update([
-                'url' => $originalImagePath
-            ]);
-        } else {
-            echo "Failed to convert image to WebP format.";
+        try {
+            if (Storage::disk('public')->exists('imagens/' . $imagem->url)) {
+                exec("cwebp $inputImagePath -o $outputImagePath");
+                Storage::disk('public')->delete($inputImagePath);
+                $imagemDB->update([
+                    'url' => $originalImagePath
+                ]);
+            }
+        } catch (\Exception $err) {
         }
     }
 
