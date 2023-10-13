2befcfacacaddf76764feb0d85ce01354b6fc798
leandrocodegit <lpoliveira.ti@gmail.com>
2023-10-13 17:22:04 -0300

    Criando a home

--- app/Http/Controllers/CatalogoController.php
+++ app/Http/Controllers/CatalogoController.php
@@ -67,7 +67,7 @@ class CatalogoController extends Controller
             'regras')
             ->where('home', true)
             ->where('active', true)
-            ->orderByRaw('RAND() LIMIT 9')
+            ->orderByRaw('RAND() LIMIT 10')
             ->get();
 
             //->unique('categoria_id')
