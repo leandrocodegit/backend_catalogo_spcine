cae43a6edfa641b70dbc41b5d6840f52c364bae9
leandrocodegit <lpoliveira.ti@gmail.com>
2023-10-27 22:15:42 -0300

    Correcao imagem principal quando vazio

--- app/Http/Controllers/CatalogoController.php
+++ app/Http/Controllers/CatalogoController.php
@@ -56,7 +56,7 @@ class CatalogoController extends Controller
             'imagens')
             ->where('home', true)
             ->where('active', true)
-            ->orderByRaw('RAND() LIMIT 11')
+            ->orderByRaw('RAND() LIMIT 13')
             ->get();
 
             //->unique('categoria_id')
